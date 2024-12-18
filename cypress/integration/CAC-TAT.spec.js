9694192774af3c99e82bb632feb48350e48ad95c
Susana Benedicto Theodoro Mendonça <susana.mendonca@entelgy.com>
2024-12-18 10:23:32 -0300

    Atualizacao

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
+    const TREE_SECONDS_IN_MS = 3000
     beforeEach(function(){
       cy.visit('./src/index.html')
     })
@@ -9,6 +10,10 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
     it('preenche os campos obrigatórios e envia o formulário', function() {
       const longtest = 'susana, susana, susana, susana, susana, susana'
+      //Com a funcinalidade cy.clock(), você pode "congelar" 🧊 o relógio do navegador.
+      //E com a funcionalidade cy.tick(), você pode avançar no tempo. 🕒
+      cy.clock() 
+
       cy.get('#firstName').type('Susana').should('have.value', 'Susana')
       cy.get('#lastName').type('Mendonca').should('have.value', 'Mendonca')  
       cy.get('#email').type('susana.teste@gmail.com').should('have.value', 'susana.teste@gmail.com') 
@@ -16,27 +21,35 @@ describe('Central de Atendimento ao Cliente TAT', function() {
       cy.get('button[type="submit"]').click()
         //cy.should('be.visible', 'Mensagem enviada com sucesso.')
       cy.get('.success').should('be.visible')
+      cy.tick(TREE_SECONDS_IN_MS)
+      cy.get('.success').should('not.be.visible')
       })
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+      cy.clock()
       cy.get('#firstName').type('Susana').should('have.value', 'Susana')
       cy.get('#lastName').type('Mendonca').should('have.value', 'Mendonca')  
       cy.get('#email').type('susana.teste').should('have.value', 'susana.teste') 
       cy.get('#open-text-area').type('susana').should('have.value', 'susana')   
       cy.contains('button', 'Enviar').click()
       cy.get('.error').should('be.visible')
+      cy.tick(TREE_SECONDS_IN_MS)
+      cy.get('.error').should('not.be.visible')
       })
     it('Campo telefone continua vazio quando insere valor difente de nro no campo', function(){
       cy.get('#phone').type('abcdfghik').should('have.value', '')
     }) 
     
     it('exibe msg de erro qdo o tel se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+      cy.clock()
       cy.get('#firstName').type('Susana').should('have.value', 'Susana')
       cy.get('#lastName').type('Mendonca').should('have.value', 'Mendonca')  
       cy.get('#email').type('susana.teste').should('have.value', 'susana.teste') 
       cy.get('#open-text-area').type('susana').should('have.value', 'susana')  
       cy.get('#phone-checkbox') .check()  
       cy.contains('button', 'Enviar').click()   
-      cy.get('.error').should('be.visible')    
+      cy.get('.error').should('be.visible')  
+      cy.tick(TREE_SECONDS_IN_MS)
+      cy.get('.error').should('not.be.visible')  
     })  
     it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
       cy.get('#firstName')
@@ -68,8 +81,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })  
     
     it('envia o formuário com sucesso usando um comando customizado', function(){
+      cy.clock()
       cy.fillMandatoryFieldsAndSubmit()
       cy.get('.success').should('be.visible')
+      cy.tick(TREE_SECONDS_IN_MS)
+      cy.get('.success').should('not.be.visible')
     })
 
     it('seleciona um produto (YouTube) por seu texto', function(){
@@ -145,12 +161,50 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('have.attr','target', '_blank')
     })
     it('acessa a página da política de privacidade removendo o target e então clicando no link', function(){
+      //Com o comando .invoke('show'), você pode forçar a exibição de um elemento HTML que esteja escondido, com um estilo display: none;, por exemplo.
+      //
       cy.get('#privacy a')
         .invoke('removeAttr', 'target')
         .click()
       cy.contains ('Talking About Testing').should('be.visible')
         
     })
-    
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
+      cy.get('.success')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Mensagem enviada com sucesso.')
+        .invoke('hide')
+        .should('not.be.visible')
+      cy.get('.error')
+        .should('not.be.visible')
+        .invoke('show')
+        .should('be.visible')
+        .and('contain', 'Valide os campos obrigatórios!')
+        .invoke('hide')
+        .should('not.be.visible')
+    })
+    it('Preencha a área de texto usando o comando invoke', function(){
+      const longText = Cypress._.repeat('Susana s', 20)
+      cy.get('#open-text-area')
+        .invoke('val', longText)
+        .should('have.value', longText)
+    })
+    it('faz uma requisição HTTP', function(){
+      cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+      .should(function(response){
+      //console.log(response)
+      const {status, statusText, body } = response
+      expect(status).to.equal(200)
+      expect(statusText).to.equal('OK')
+      expect(body).to.include('CAC TAT')
+      })
+    })
+    it('Encontra o gato escondido - bem escondido', function(){
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+    })
 })
 
--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,4 +1,7 @@
-it('testa a página da política de privacidade de forma independente', function(){
-cy.visit('./src/privacy.html')
-cy.contains ('Talking About Testing').should('be.visible')
+Cypress._.times(3, function() {
+  //A funcionalidade _.times() serve para você executar uma função de callback um certo número de vezes, onde o número de vezes é o primeiro argumento, e a função de callback é o segundo.
+    it('testa a página da política de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
+        cy.contains ('Talking About Testing').should('be.visible')
+        })
 })
